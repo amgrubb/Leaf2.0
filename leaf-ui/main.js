87a450c8dd6936355590c69e6280773942955c4e
Charlie Chenyu Zhang <charlie.chenyu.zhang@gmail.com>
2018-08-26 16:41:28 -0400

    foward analysis works as expected. Need further test it out

--- leaf-ui/main.js
+++ leaf-ui/main.js
@@ -30,6 +30,15 @@ var satValueDict = {
 	"conflict": 4,
 	"none": 6
 };
+var satValueDictInverse = {
+	5: "unknown",
+	3: "satisfied",
+	2: "partiallysatisfied",
+	1: "partiallydenied",
+	0: "denied",
+	4: "conflict",
+	6: "none"
+};
 var weightedContDict = {
 	"makes": 0,
 	"helps": 1,
@@ -1002,9 +1011,29 @@ $('#frd-analysis-btn').on('click', function(){
 		var satisfactionValue = calculateEvaluation(elements, savedLinks, element);
 		
 		
-
-
+		
+		
+		
 		// TODO: update the satisfactionValue of the element and udpate the graph
+		
+		console.log(satisfactionValue);
+		if (satisfactionValue > 0){
+			updateValues(element, satValueDictInverse[satisfactionValue]);
+		}
+		
+
+		// var elements = graph.getElements();
+		// for (var i = 0; i < elements.length; i++){
+		// 	elements[i].removeAttr(".satvalue/d");
+		// 	elements[i].attr(".constraints/lastval", "none");
+		// 	elements[i].attr(".funcvalue/text", " ");
+		// 	var cellView  = elements[i].findView(paper);
+		// 	elementInspector.render(cellView);
+		// 	elementInspector.$('#init-sat-value').val("none");
+		// 	elementInspector.updateHTML(null);
+		// }
+
+
 
 
 
@@ -1034,46 +1063,7 @@ $('#frd-analysis-btn').on('click', function(){
 				}
 			}
 		}
-		
-			// only remove the eleDest from the elementsWaiting array iff we have evaluated all elements whose parents is eleDest
 	}
-
-		// IMPORTANT - useful stuff
-		// console.log(elements[e].attr(".satvalue/value")); // how you get the satisfaction value of the element
-		// elements[e].attr(".name/text").replace(/\n/g, " ") // how you get the name of each element
-		// ways to tell the intention type of each element
-		// if (elements[e] instanceof joint.shapes.basic.Goal)
-		// 	  console.log("G\t");	  
-		// else if (elements[e] instanceof joint.shapes.basic.Task)
-		// 	console.log("T\t");
-		// else if (elements[e] instanceof joint.shapes.basic.Quality)
-		// 	console.log("Q\t"); // NOTE this is different in other tools, in other tools, we use S to represent soft goals
-		// else if (elements[e] instanceof joint.shapes.basic.Resource)
-		// 	console.log("R\t");
-		// else
-		// 	console.log("I\t");
-	// }
-	// print out all the relationship links
-	// for (var l = 0; l < savedLinks.length; l++){
-	// 	var current = savedLinks[l];
-	// 	var relationship = current.label(0).attrs.text.text.toUpperCase()
-	// 	var source = "-";
-	// 	var target = "-";
-	// 	// distinguish the source and target
-	// 	if (current.get("source").id)
-	// 		source = graph.getCell(current.get("source").id).prop("elementid");
-	// 	if (current.get("target").id)
-	// 		target = graph.getCell(current.get("target").id).prop("elementid");
-	// 	// console.log('relationship: ' + relationship);
-	// 	if (relationship.indexOf("|") > -1){
-	// 		evolvRelationships = relationship.replace(/\s/g, '').split("|");
-	// 		console.log('L\t' + evolvRelationships[0] + '\t' + source + '\t' + target + '\t' + evolvRelationships[1] + "\n");
-	// 	}else{
-	// 		console.log('L\t' + relationship + '\t' + source + '\t' + target + "\n");
-	// 	}
-	// }
-
-	// Initialise values
 });
 
 // calculate evaluation
@@ -1107,13 +1097,13 @@ function calculateEvaluation(elements, savedLinks, element) {
 		preSums[i] = 0;
 	}
 	
-	//Go through all links that the current node is the source of
+	//Go through all links that the current node is the target of
 	var linksWanted = [];
 	for (var l = 0; l < savedLinks.length; l++){
 		var current = savedLinks[l]; // current is each link
-		if (current.get("source").id && current.get("source").id == element.id && current.get("target").id){
+		if (current.get("target").id && current.get("target").id == element.id && current.get("source").id){
 			linksWanted.push(current);
-		} else if (current.label(0).attrs.text.text == "depends" && current.get("target").id == element.id){
+		} else if (current.label(0).attrs.text.text == "depends" && current.get("source").id == element.id){
 			// with exception of dependency where source is the dependerElmt, target is the dependum/dependeeElmt
 			linksWanted.push(current);
 		}
@@ -1121,9 +1111,9 @@ function calculateEvaluation(elements, savedLinks, element) {
 	// get the current evaluation value of the source and target along with the link label
 	for (var l = 0; l < linksWanted.length; l++){
 		var eachLink = linksWanted[l];
-		var targetNode = getTarget(elements, eachLink.get("target").id);
-		var tVal = satValueDict[targetNode.attr(".satvalue/value")]; // satisfaction value of the target node
-		var sVal = satValueDict[element.attr(".satvalue/value")]; // satisfaction value of the source node
+		var sourceNode = getSource(elements, eachLink.get("source").id);
+		var sVal = satValueDict[sourceNode.attr(".satvalue/value")]; // satisfaction value of the target node
+		// var tVal = satValueDict[element.attr(".satvalue/value")]; // satisfaction value of the source node
 		// four cases that we are consiedering here
 		// decomposition (and, or)
 		if (eachLink.label(0).attrs.text.text == "and" || eachLink.label(0).attrs.text.text == "or"){
@@ -1140,28 +1130,33 @@ function calculateEvaluation(elements, savedLinks, element) {
 			hasDependencies = true;
 			dependSums[sVal] ++;
 		} else {
-			// TODO: precondition, need to fix the condition
+			// TODO: need to figure out what kind of condition is considered as Precondition, fix the condition accordingly
 			hasPreconditions = true;
 			preSums[sVal]++;
 		}
-		
-		var result = -1;
-		if (hasDecomposition){
-			result = getDecomposition(decomSums, eachLink.label(0).attrs.text.text);
-		}
-			
-		if (numContributions > 0) {
-			if (hasDecomposition)
-				sums[result]++;
-			result = getQualitativeContribution(sums, numContributions);
-		}
-		
-		if (hasPreconditions){
-			result = getPrecondition(preSums, result);
-		}
-		
-		return result;	
 	}
+		
+	var result = -1;
+	if (hasDecomposition){
+		result = getDecomposition(decomSums, eachLink.label(0).attrs.text.text);
+	}
+		
+	if (numContributions > 0) {
+		if (hasDecomposition)
+			sums[result]++;
+		result = getQualitativeContribution(sums, numContributions);
+	}
+	
+	if (hasDependencies){
+		if (hasDecomposition || numContributions > 0) // Since result will be none we shouldn't need this condition, just the next statement.
+			dependSums[result]++;		// Add previous result to the dependSum.
+		result = getDecomposition(dependSums, "and");
+	}
+
+	if (hasPreconditions){
+		result = getPrecondition(preSums, result);
+	}
+	return result;
 }
 
 function getDecomposition(decomSums, type){
@@ -1304,10 +1299,35 @@ function inElementsWaiting(elementsWaiting, target) {
 	
 }
 
-function getTarget(elements, targetID){
+function getSource(elements, sourceID){
 	for (var l = 0; l < elements.length; l++){
-		if (elements[l].id == targetID){
+		if (elements[l].id == sourceID){
 			return elements[l];
 		}
+	}
+}
+
+//Update the satisfaction value of a particular node in the graph
+function updateValues(cell, value){
+	var value;
+	//Update images for properties
+	if (value == "satisfied"){
+		cell.attr({ '.satvalue': {'d': 'M 0 10 L 5 20 L 20 0 L 5 20 L 0 10', 'stroke': '#00FF00', 'stroke-width':4, 'value':value}});
+	}else if(value == "partiallysatisfied") {
+		cell.attr({ '.satvalue': {'d': 'M 0 8 L 5 18 L 20 0 L 5 18 L 0 8 M 17 30 L 17 15 C 17 15 30 17 18 23', 'stroke': '#00FF00', 'stroke-width':4, 'fill':'transparent', 'value':value}});
+	}else if (value == "denied"){
+		cell.attr({ '.satvalue': {'d': 'M 0 20 L 20 0 M 10 10 L 0 0 L 20 20', 'stroke': '#FF0000', 'stroke-width': 4, 'value':value}});
+	}else if (value == "partiallydenied") {
+		cell.attr({ '.satvalue': {'d': 'M 0 15 L 15 0 M 15 15 L 0 0 M 17 30 L 17 15 C 17 15 30 17 18 23', 'stroke': '#FF0000', 'stroke-width': 4, 'fill': 'transparent', 'value':value}});
+	}else if (value == "conflict") {
+		cell.attr({ '.satvalue': {'d': 'M 0 0 L 20 8 M 20 7 L 5 15 M 5 14 L 25 23', 'stroke': '#222222', 'stroke-width': 4, 'value':value}});
+	}else if (value == "unknown") {
+		cell.attr({ '.satvalue': {'d': 'M15.255,0c5.424,0,10.764,2.498,10.764,8.473c0,5.51-6.314,7.629-7.67,9.62c-1.018,1.481-0.678,3.562-3.475,3.562\
+		    c-1.822,0-2.712-1.482-2.712-2.838c0-5.046,7.414-6.188,7.414-10.343c0-2.287-1.522-3.643-4.066-3.643\
+		    c-5.424,0-3.306,5.592-7.414,5.592c-1.483,0-2.756-0.89-2.756-2.584C5.339,3.683,10.084,0,15.255,0z M15.044,24.406\
+		    c1.904,0,3.475,1.566,3.475,3.476c0,1.91-1.568,3.476-3.475,3.476c-1.907,0-3.476-1.564-3.476-3.476\
+		    C11.568,25.973,13.137,24.406,15.044,24.406z', 'stroke': '#222222', 'stroke-width': 10, 'value':value}});
+	}else {
+		cell.removeAttr(".satvalue/d");
 	}
 }
